38bf905dcb34d2327b8257ebe715a63794443f4c
Cho Seong week <tjddnr0217@gmail.com>
2022-12-01 22:14:31 +0900

    feat : 리액트 쿼리 관련 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import styled from '@emotion/styled/macro';
-import SkeletonList from './pages/SkeletonList';
-import InfinityScroll from './component/InfinityScroll';
+import React from "react";
+import styled from "@emotion/styled/macro";
+import SkeletonList from "./pages/SkeletonList";
+import InfinityScroll from "./component/InfinityScroll";
+import Example from "./component/ReactQueryExample/Example";
 
 function App() {
   return (
     <div>
-      <InfinityScroll />
+      <Example />
     </div>
   );
 }
